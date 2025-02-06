14684f58c149e4e6bea7965c8f9f062446af801f
eazyposy <thakur.raxit@gmail.com>
2025-02-06 17:00:50 +0530

    Prod Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { images: {
+    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/deab9bxab/**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },};
 
 export default nextConfig;
---
+++ src/middleware/middleware.js
@@ -0,0 +1,13 @@
+// src/middleware.js
+
+// Middleware no longer required as authorization is removed.
+// You can either delete this file or leave it empty.
+
+// Optionally, if you have other middleware logic, retain it here.
+
+// Example: Empty middleware
+export function middleware(req) {
+    return NextResponse.next();
+  }
+  
+  export const config = {};
